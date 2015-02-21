81718878932d2e7b38ce04f8bd6d8b64ab346093
J.T. Halbert <b3d3vil3d@gmail.com>
2015-02-21 12:42:10 -0500

    added spark-shell start to script.

--- EMR/setup.sh
+++ EMR/setup.sh
@@ -20,5 +20,28 @@ echo "pulling down some utilities (Thanks Markus!!!! Again!!!)"
 aws s3 cp s3://morris-datasets/ENRON/demo/log4j.properties .
 aws s3 cp s3://morris-datasets/ENRON/demo/mailrecord-utils-0.9.0-SNAPSHOT-shaded.jar .
 
-set SPARK_HOME=spark-1.2.1-hadoop2.4
-set PATH=$SPARK_HOME/bin:$PATH
+export SPARK_HOME=spark-1.2.1-hadoop2.4
+export PATH=$SPARK_HOME/bin:$PATH
+
+echo
+echo
+echo "Starting Spark with "
+echo "spark-shell --master yarn --driver-memory 4G --executor-memory 4G \ "
+echo "    --conf spark.serializer=org.apache.spark.serializer.KryoSerializer \ "
+echo "    --conf spark.kryo.registrator=com.uebercomputing.mailrecord.MailRecordRegistrator \ "
+echo "    --conf spark.kryoserializer.buffer.mb=128 \ "
+echo "    --conf spark.kryoserializer.buffer.max.mb=512 \ "
+echo "    --jars mailrecord-utils-0.9.0-SNAPSHOT-shaded.jar\ "
+echo "    --driver-java-options "-Dlog4j.configuration=log4j.properties" "
+echo
+echo
+echo "Have fun!"
+
+spark-shell --master yarn --driver-memory 4G --executor-memory 4G \
+    --conf spark.serializer=org.apache.spark.serializer.KryoSerializer \
+    --conf spark.kryo.registrator=com.uebercomputing.mailrecord.MailRecordRegistrator \
+    --conf spark.kryoserializer.buffer.mb=128 \
+    --conf spark.kryoserializer.buffer.max.mb=512 \
+    --jars mailrecord-utils-0.9.0-SNAPSHOT-shaded.jar\
+    --driver-java-options "-Dlog4j.configuration=log4j.properties"
+
